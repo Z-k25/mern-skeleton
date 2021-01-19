633c298212d59a67f148a5cd9c221d2a2c572569
Z-k25 <ivanov.i.a@pm.me>
2021-01-19 19:26:26 +0300

    correct-server-dist-files

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -26,7 +26,7 @@ const getUniqueErrorMessage = err => {
         let fieldName = err.message.substring(err.message.lastIndexOf('.$') + 2,
             err.message.lastIndexOf('_1'))
         output = fieldName.charAt(0).toUpperCase() + fieldName.slice(1) +
-            'already exists'
+            ' already exists'
     } catch (ex) {
         output = 'Unique field already exists'
     }
--- server/server.js
+++ server/server.js
@@ -1,26 +1,17 @@
 import config from './../config/config'
 import app from './express'
 import mongoose from 'mongoose'
-import Template from './../template'
 
+// Connection URL
 mongoose.Promise = global.Promise
-mongoose.connect(config.mongoUri, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-})
-
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 mongoose.connection.on('error', () => {
-    throw new Error(`unable to connect to database: ${mongoUri}`)
+  throw new Error(`unable to connect to database: ${config.mongoUri}`)
 })
 
-app.get('/', (req, res) => {
-    res.status(200).send(Template())
-})
-
-app.listen(config.port, err => {
-    if (err) {
-        console.log(err)
-    }
-    console.info('Server started on port %s', config.port)
+app.listen(config.port, (err) => {
+  if (err) {
+    console.log(err)
+  }
+  console.info('Server started on port %s.', config.port)
 })
